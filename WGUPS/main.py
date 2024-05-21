c9cfa11f6d9c147f96a5c458a9996dbbe79f89b2
Jaxon Adams <jaxon.adams@loanpro.io>
2024-05-21 13:08:55 -0600

    initial main.py setup

---
+++ WGUPS/main.py
@@ -0,0 +1,34 @@
+# JAXON ADAMS -- STUDENT ID: 011441603
+
+"""C950 - Data Structures & Algorithms II - Performance Assessment
+
+WGUPS Routing Program implementation. Uses a hash table as the self-adjusting
+data structure required for storing package data. The algorithm used to plot
+truck delivery routes is a greedy algorithm called the "nearest neighbor"
+algorithm.
+
+Author: Jaxon Adams
+Student ID: 011441603
+"""
+
+
+class Simulation:
+    """Creates and manages the simulation of a package delivery routing
+    program. Run the simulation by invoking the "run" method.
+    """
+
+    def __init__(self):
+    
+        pass
+
+    def run(self):
+
+        print("Hello, world!")
+
+
+# !---------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    program = Simulation()
+
+    program.run()
