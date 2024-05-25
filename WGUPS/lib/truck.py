05d02a970728acfe3add891afcbf4f19efc012f4
Jaxon Adams <jadamsresume1@gmail.com>
2024-05-25 14:11:43 -0600

    began work on simulation code

--- WGUPS/lib/truck.py
+++ WGUPS/lib/truck.py
@@ -4,6 +4,12 @@ neighbor algorithm.
 """
 
 
+import collections
+
+
+Event = collections.namedtuple("Event", "time truck_id dist_travelled action")
+
+
 class Truck:
 
     def __init__(self, id, address_list, distance_matrix):
@@ -60,7 +66,7 @@ class Truck:
         for pkg_id in self.packages:
             
             package = package_table.lookup(pkg_id)
-            unvisited_addr.append(package.address)
+            unvisited_addr.append((pkg_id, package.address))
 
         distance_traveled = 0
 
@@ -68,28 +74,46 @@ class Truck:
         while len(unvisited_addr) > 0:
             next_address = None
             min_distance = float("inf")
+            next_pkg_id = None
 
-            for address in unvisited_addr:
+            for pkg_id, address in unvisited_addr:
                 distance = self.lookup_distance(location, address)
                 if distance < min_distance:
                     min_distance = distance
                     next_address = address
+                    next_pkg_id = pkg_id
             
-            self.route.append(next_address)
+            self.route.append((next_address, min_distance, next_pkg_id))
             unvisited_addr.pop(
-                unvisited_addr.index(next_address)
+                unvisited_addr.index((next_pkg_id, next_address))
             )
             location = next_address
             distance_traveled += min_distance
 
         # return to hub after packages are delivered
-        self.route.append("4001 South 700 East, Salt Lake City, UT 84107")
-        distance_traveled += self.lookup_distance(
+        dist_to_hub = self.lookup_distance(
             location,
             "4001 South 700 East, Salt Lake City, UT 84107"
         )
+        
+        self.route.append(("4001 South 700 East, Salt Lake City, UT 84107", dist_to_hub, None))
+        distance_traveled += dist_to_hub
 
         return distance_traveled
+
+    def deliver_packages(self, package_table, start_time=0):
+        """Deliver packages by visiting each address in the plotted route.
+        Yield to the simulator issuing events to allow other trucks to deliver
+        packages as well.
+        """
+
+        time = yield Event(start_time, self.id, 0, "Leaving the HUB")
+        for address, distance, pkg_id in self.route:
+            # TODO: handle package delivery
+            if pkg_id is not None:
+                pkg = package_table.lookup(pkg_id)
+                pkg.status = f"DELIVERED {time}"
+            time = yield Event(time, self.id, distance, f"Dropped off package at {address}")
 
 
 # !---------------------------------------------------------------------------
